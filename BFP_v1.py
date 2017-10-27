569598c3bee4894807bf3d844ec91b68deefd76f
Gowtham Kuntumalla <gowthamkuntumalla@gmail.com>
2017-10-27 22:33:31 +0530

    initial files

---
+++ BFP_v1.py
@@ -0,0 +1,16 @@
+#! usr/bin/env python
+
+# Copyright 2017 Gowtham Kuntumalla
+# File name: BFP_v1.py
+
+import numpy as np
+
+# LIVE BITCOIN PRICES -First task, store it in locally for further use             ------ Web to local (data transfer)
+
+# ANALYSIS OF AVAILABLE DATA - Fourier transform in different time intervals.      ------ Data Analysis + ML
+
+# LIVE NEWS ANALYSIS- Generic code to analyze anywebsite entered herewith          ------ ML
+
+# PREDICTIONS - Use signal processing theory to extract various frequencies and in varying time scales. Chaos theory is used to predict guess the changing attractors.
+
+## END OF CODE ##
